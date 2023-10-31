20648f091704ba0e6c327455e25023b37904e9f4
Eldryn Santos <eldryn.dev@gmail.com>
2023-10-31 15:25:26 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/graminha-do-bem/',
+  build: {
+    outDir: 'dist' // Nome da pasta de saída, por padrão é 'dist'
+  }
+});
